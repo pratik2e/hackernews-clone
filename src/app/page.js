883ad699eb03672befb159300d9bd7892d1d2c30
pratikshit <pratikshitkushwaha08@gmail.com>
2024-01-15 20:08:08 +0530

    updated loader

--- src/app/page.js
+++ src/app/page.js
@@ -10,13 +10,16 @@ import {
   PaginationNext,
   PaginationPrevious,
 } from "@/components/ui/pagination";
+import { Progress } from "@/components/ui/progress";
 
 const Page = () => {
   const [stories, setStories] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
+  const [loading, setLoading] = useState(true);
   const storiesPerPage = 30;
 
   useEffect(() => {
+    setLoading(true);
     const startIndex = (currentPage - 1) * storiesPerPage;
     const endIndex = startIndex + storiesPerPage;
 
@@ -31,7 +34,7 @@ const Page = () => {
         const topStoryIds = response.data; // Array of top story IDs
 
         // Let's just display the IDs of the first 30 stories
-          const currentStories = topStoryIds.slice(startIndex, endIndex);
+        const currentStories = topStoryIds.slice(startIndex, endIndex);
 
         // Now, fetch details for each story and store them in state
         const storyPromises = currentStories.map((storyId) =>
@@ -45,20 +48,30 @@ const Page = () => {
             // Handle successful story details responses
             const storiesDetails = storiesData.map((story, index) => {
               const { id, by: author, time, title, url } = story.data;
-              return { id, author, time, title, url, index: index + startIndex + 1 };
+              return {
+                id,
+                author,
+                time,
+                title,
+                url,
+                index: index + startIndex + 1,
+              };
             });
             setStories(storiesDetails);
+            setLoading(false);
           })
           .catch((error) => {
             // Handle error fetching story details
             console.error("Error fetching story details:", error);
+            setLoading(false);
           });
       })
       .catch((error) => {
         // Handle error fetching top stories
         console.error("Error fetching top stories:", error);
+        setLoading(false);
       });
-  }, [currentPage,storiesPerPage]);
+  }, [currentPage, storiesPerPage]);
 
   const handlePreviousPage = () => {
     if (currentPage > 1) {
@@ -78,42 +91,43 @@ const Page = () => {
           borderRadius: "var(--radius-3)",
         }}
       >
-        {stories.map((story) => (
-          <div key={story.id} className="space-y-2 rounded-md p-4 py-4">
-            <Container size="3">
-            <p className="text-lg font-bold">{'Sr. No: ' + story.index}</p>
-              <Text
-                as="div"
-                size="3"
-                weight="bold"
-                className="text-lg font-bold mb-2"
-              >
-                <a href={story.url} target="_blank" rel="noopener noreferrer">
-              {story.title}
-            </a>
-              </Text>
-              <p>{new Date(story.time * 1000).toLocaleString()}</p>
-              <p>{story.author}</p>
-              <p>
-              <a href={story.url} target="_blank" rel="noopener noreferrer">
-                 {story.url}
-               </a>
-              </p>
-              <hr />
-            </Container>
-          </div>
-        ))}
+        {loading && <Progress value={80} />}
+        {!loading &&
+          stories.map((story) => (
+            <div key={story.id} className="space-y-2 rounded-md p-4 py-4">
+              <Container size="3">
+                <p className="text-lg font-bold">{"Sr. No: " + story.index}</p>
+                <Text
+                  as="div"
+                  size="3"
+                  weight="bold"
+                  className="text-lg font-bold mb-2"
+                >
+                  <a href={story.url} target="_blank" rel="noopener noreferrer">
+                    {story.title}
+                  </a>
+                </Text>
+                <p>{new Date(story.time * 1000).toLocaleString()}</p>
+                <p>{story.author}</p>
+                <p>
+                  <a href={story.url} target="_blank" rel="noopener noreferrer">
+                    {story.url}
+                  </a>
+                </p>
+                <hr />
+              </Container>
+            </div>
+          ))}
       </Box>
       <div>
-      <Pagination>
-  <PaginationContent>
-      <PaginationPrevious onClick={handlePreviousPage}/>
-      <PaginationLink>{currentPage}</PaginationLink>
-      <PaginationEllipsis />
-      <PaginationNext onClick={handleNextPage} />
-  </PaginationContent>
-</Pagination>
-
+        <Pagination>
+          <PaginationContent>
+            <PaginationPrevious onClick={handlePreviousPage} />
+            <PaginationLink>{currentPage}</PaginationLink>
+            <PaginationEllipsis />
+            <PaginationNext onClick={handleNextPage} />
+          </PaginationContent>
+        </Pagination>
       </div>
     </div>
   );
